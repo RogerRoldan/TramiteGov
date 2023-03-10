623a3ce623bd1061ce2637a2f6b68e169d95cd27
Roger Roldan <rogerricardo09@gmail.com>
2023-03-10 12:13:42 -0500

    implement get and post file with paramters id instance and name variable

--- Controllers/Variables.cs
+++ Controllers/Variables.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using System.Net;
 using System.Text;
 using TramiteGov.Environtment;
 using TramiteGov.Models;
@@ -55,5 +56,54 @@ namespace TramiteGov.Controllers
 
             return Content(Response, "application/json", Encoding.UTF8);
         }
+        //Upload File Camunda
+        [HttpPost("uploadfile/{idInstanced}/{nameVariable}")]
+        public IActionResult PostUploadFile(string idInstanced,string nameVariable ,[FromForm] IFormFile file)
+        {
+            string Url = BaseUrl + idInstanced + "/variables/" + nameVariable + "/data";
+            client.BaseAddress = new Uri(Url);
+
+            var streamContent = new StreamContent(file.OpenReadStream());
+            string extension = Path.GetExtension(file.FileName);
+            
+            var content = new MultipartFormDataContent();
+            content.Add(streamContent, "data", nameVariable + extension);
+            content.Add(new StringContent("File"), "valueType");
+
+            var response = client.PostAsync("", content).Result;
+            var responseContent = response.Content.ReadAsStringAsync().Result;
+
+
+            return Ok(responseContent);
+        }
+
+        //obtener variable de archivo
+        [HttpGet("getfile/{idInstanced}/{nameVariable}")]
+        public IActionResult GetFile(string idInstanced, string nameVariable)
+        {
+            string Url = BaseUrl + idInstanced + "/variables/" + nameVariable + "/data";
+            var Response = client.GetAsync(Url).Result;
+            
+            if (Response.StatusCode == HttpStatusCode.OK)
+            {
+                var nameFile = Response.Content.Headers.ContentDisposition.FileNameStar;
+                string extension = Path.GetExtension(nameFile);
+
+                var ResponseContent = Response.Content.ReadAsStreamAsync().Result;
+
+                return File(ResponseContent, "application/octet-stream", nameFile);
+            }
+            else
+            {
+                return BadRequest();
+            }
+
+
+        }
     }
 }
+
+
+//string path = @"C:\Users\Roger Roldan\Downloads\arc.txt";
+//using var fileStream = new FileStream(path, FileMode.Open, FileAccess.Read);
+//using var streamContent = new StreamContent(fileStream);
